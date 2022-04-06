68e6fd0d744f3e062bf9f76a5d8f7dc7f28c5f8f
yuhyeon <uvaaaan0809@naver.com>
2022-04-06 18:01:48 +0900

    게시글 등록 기능, html 추가

--- src/main/java/yylab/findMy/web/posts/PostsController.java
+++ src/main/java/yylab/findMy/web/posts/PostsController.java
@@ -1,6 +1,7 @@
 package yylab.findMy.web.posts;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,7 +10,7 @@ import yylab.findMy.domain.posts.Posts;
 import yylab.findMy.service.PostsService;
 
 @RequiredArgsConstructor
-@RestController
+@Controller
 public class PostsController {
 
     private final PostsService postsService;
